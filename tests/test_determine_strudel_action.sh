1c5f397973bedfdcbf7f1089ff295ac2078e00d6
shai <shai_rubin@yahoo.com>
2024-09-26 08:59:06 +0300

    first test calling the script 2

--- tests/test_determine_strudel_action.sh
+++ tests/test_determine_strudel_action.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-output=$(../determine_strudfel_action.sh "strudel add logs")
+output=$(../determine_strudel_action.sh "strudel add logs")
 if [ "$output" != "add-logs" ]; then
   echo "Test failed add-logs!"
   exit 1
 fi
-output=$(../determine_strudfel_action.sh "remove strudel add logs")
+output=$(../determine_strudel_action.sh "remove strudel add logs")
 if [ "$output" != "none" ]; then
   echo "Test failed remove and add together"
   exit 1
 fi
-output=$(../determine_strudfel_action.sh "remove logs strudel")
+output=$(../determine_strudel_action.sh "remove logs strudel")
 if [ "$output" != "remove-logs" ]; then
   echo "Test failed remove-logs"
   exit 1
