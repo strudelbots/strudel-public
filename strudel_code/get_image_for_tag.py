4ba4275abdf5bec0a89fb9034c24d6cab2fe39d5
strudel-logger <strudel-logger@users.noreply.github.com>
2024-11-23 07:53:29 +0000

    Automated strudel-logger commit for branch release-0.13

--- strudel_code/get_image_for_tag.py
+++ strudel_code/get_image_for_tag.py
@@ -1,16 +1,22 @@
+import logging # STRUDEL_IMPORT_0
 import sys
 
 from strudel_code.image_map_data_classes import verify_release_tag, load_current_map
 
 #v0.13.02
+strudel = logging.getLogger(__name__) # STRUDEL_IMPORT_1
+strudel.addHandler(logging.StreamHandler()) # STRUDEL_IMPORT_2
+strudel.setLevel(logging.INFO) # STRUDEL_IMPORT_3
 def get_image_for_tag(release_tag):
     map = load_current_map()
     result = map.find_image(release_tag)
     if not result:
+        strudel.info(' "result" is evaluated to False') #  # STRUDEL_IF_LOG_0
         for entry in map.release_details:
             if release_tag.startswith(entry.release_tag):
 #                strudel.info(f"Found release tag: {release_tag}")
                 return entry.image[0]
+    strudel.info('Method "get_image_for_tag" returns "result"') #  # STRUDEL_RETURN_TRACE_0
     return result
 
 
@@ -32,6 +38,7 @@ if __name__ == '__main__':
         print("no-tag-found", file=sys.stdout)
     else:
         print(image_tag.tag, file=sys.stdout)
+
 
 
 
