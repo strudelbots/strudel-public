4391478724605726ca4a439b791f889780f6a426
shai <shai_rubin@yahoo.com>
2024-12-14 12:11:17 +0200

    removing deleted files

--- strudel_code/functions_for_analyzing_git_db.sh
+++ strudel_code/functions_for_analyzing_git_db.sh
@@ -198,9 +198,10 @@ file_exists_in_branch() {
     echo "File name: $file_name"
     # Check if the file exists in the branch or if it was deleted
     # The command lists all files in the branch and also checks for files that are marked as deleted in git
-    sleep 15
+    sleep 3
     echo "after sleep"
     git log --name-status $branch_name -- $file_name
+    echo "after log"
     #(git log --name-status $branch_name -- $file_name | grep -q $'^D[ \t]')
     #    deleted=$?
     #    # If the file is marked as deleted in the branch, it does not exists
