0e3a4e4a18df851401dee06eb566327112e77ae3
shai <shai_rubin@yahoo.com>
2024-12-14 10:06:05 +0200

    fixed file name

--- strudel_code/functions_for_analyzing_git_db.sh
+++ strudel_code/functions_for_analyzing_git_db.sh
@@ -173,6 +173,7 @@ last_common_commit() {
 
     local branch1="$1"
     local branch2="$2"
+    echo
     echo "Branch1: $branch1"
     echo "Branch2: $branch2"
     # Find the merge base (common ancestor) of the two branches
