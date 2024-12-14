cd0cc64fb7c89cb2f27c6f736985e86680408200
shai <shai_rubin@yahoo.com>
2024-12-14 11:11:18 +0200

    added git fetch on origin

--- strudel_code/functions_for_analyzing_git_db.sh
+++ strudel_code/functions_for_analyzing_git_db.sh
@@ -177,7 +177,7 @@ last_common_commit() {
     echo "Branch2: $branch2" >> /tmp/commit_functions.log
     # Find the merge base (common ancestor) of the two branches
     git checkout $branch1 >> /tmp/commit_functions.log
-    git checkout 39-add-single-developer-mode-for-selecting-the-files-on-which-strudel-will-work-on >> /tmp/commit_functions.log
+    git checkout $branch2 >> /tmp/commit_functions.log
     sleep 1
     # git merge-base origin/main "$branch2" >> /tmp/commit_functions.log
     git merge-base main 39-add-single-developer-mode-for-selecting-the-files-on-which-strudel-will-work-on
