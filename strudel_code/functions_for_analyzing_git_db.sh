8fdccc12a1964be039eadfccd687aadda692a5d4
shai <shai_rubin@yahoo.com>
2024-12-14 10:50:34 +0200

    added git fetch on origin

--- strudel_code/functions_for_analyzing_git_db.sh
+++ strudel_code/functions_for_analyzing_git_db.sh
@@ -176,6 +176,7 @@ last_common_commit() {
     echo "Branch1: $branch1" >> /tmp/commit_functions.log
     echo "Branch2: $branch2" >> /tmp/commit_functions.log
     # Find the merge base (common ancestor) of the two branches
+    git fetch origin
     git merge-base "$branch1" "$branch2" >> /tmp/commit_functions.log
     git merge-base "$branch1" "$branch2"
 }
