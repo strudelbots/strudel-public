c6b154038060e324782ea8cee7213c6b7bf80dc9
shai <shai_rubin@yahoo.com>
2024-12-14 16:44:30 +0200

    add strudel logs

--- devops_scripts/start_release.sh
+++ devops_scripts/start_release.sh
@@ -13,12 +13,14 @@ if [ -z "$2" ]; then
 fi
 old_tag=$1
 new_tag=$2
+branch=$(git rev-parse --abbrev-ref HEAD)
 echo "old tag is: $old_tag"
 echo "new tag is: $new_tag"
 cd ../.github/workflows
 pwd
 find . -name "*.yml" -exec sed -i s/$old_tag/$new_tag/g {} \;
 git commit -am "Update tags in workflows" -m "$new_tag"
-
+git tag $new_tag
+git push --atomic origin  $branch $new_tag
 
 
