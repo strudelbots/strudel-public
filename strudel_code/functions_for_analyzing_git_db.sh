54ccc4f30b5b54a86cac9f8c037829a730775b2b
shai <shai_rubin@yahoo.com>
2024-12-14 10:03:23 +0200

    fixed file name

--- strudel_code/functions_for_analyzing_git_db.sh
+++ strudel_code/functions_for_analyzing_git_db.sh
@@ -176,7 +176,7 @@ last_common_commit() {
     echo "Branch1: $branch1"
     echo "Branch2: $branch2"
     # Find the merge base (common ancestor) of the two branches
-    git merge-base "$branch1" "$branch2"
+    #git merge-base "$branch1" "$branch2"
 }
 commits_after_common_commit() {
     if [ "$#" -ne 2 ]; then
