8d77254b14666711290053630178c0eaeb29ec12
shai <shai_rubin@yahoo.com>
2024-12-18 13:35:17 +0200

    testing with real input 5

--- strudel_code/strudel_cli.sh
+++ strudel_code/strudel_cli.sh
@@ -6,4 +6,4 @@ gh api \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   /repos/strudelbots/strudel-public/actions/workflows/client_side_strudel_test.yml/dispatches \
-   -f "ref=44-run-addremove-logs-with-gh-cli" -f "inputs[user_command]=add-logs"
+   -f "ref=44-run-addremove-logs-with-gh-cli" -f "inputs[user_command]=strudel-test"
