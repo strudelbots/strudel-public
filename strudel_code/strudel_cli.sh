cc6756dbb452e4404aa25bbb89b5ce227dc15d35
shai <shai_rubin@yahoo.com>
2024-12-18 14:45:34 +0200

    better cli

--- strudel_code/strudel_cli.sh
+++ strudel_code/strudel_cli.sh
@@ -36,10 +36,10 @@ if ! is_valid_arg "$ARG"; then
     echo "Valid values are: ${VALID_ARGS[*]}"
     exit 1
 fi
-
+branch=$(bit rev-parse --abbrev-ref HEAD)
 gh api \
   --method POST \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   /repos/strudelbots/strudel-public/actions/workflows/client_side_strudel_test.yml/dispatches \
-   -f "ref=44-run-addremove-logs-with-gh-cli" -f "inputs[user_command]=teststrudel"
+   -f "$branch" -f "inputs[user_command]=test-strudel"
