c42cabe772760fc5b3400f4e08f321172c7ce77d
shai <shai_rubin@yahoo.com>
2024-12-18 11:25:57 +0200

    add user command input

---
+++ strudel_code/strudel_cli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Hello from Strudel CLI"
+
+gh api \
+  --method POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  /repos/strudelbots/strudel-public/actions/workflows/client_side_strudel_test.yml/dispatches \
+   -f "ref=44-run-addremove-logs-with-gh-cli" -f "inputs[user_command]=add-logs"
