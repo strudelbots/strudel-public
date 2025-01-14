62eecbe090ac9a8fce3970668022432baa5ae2ad
shai <shai_rubin@yahoo.com>
2025-01-14 11:47:18 +0200

    test-strudel change defailt image

--- strudel_code/get_image_for_tag.py
+++ strudel_code/get_image_for_tag.py
@@ -7,7 +7,6 @@ from strudel_code.image_map_data_classes import verify_release_tag, load_current
 def get_image_for_tag(release_tag):
     if release_tag ==  'run-entire-repository':
         release_tag =  "v0.22.02" # TODO - generalize
-    raise ValueError(f"do we get here 2 {release_tag}")
     map = load_current_map()
     result = map.find_image(release_tag)
     if not result:
--- strudel_code/image_map_data_classes.py
+++ strudel_code/image_map_data_classes.py
@@ -37,6 +37,7 @@ class ImageMap:
         if not release_tag:
             strudel.info(f' Return None because "release_tag" is evaluated to False') #  # STRUDEL_IF_LOG_1
             return None
+        raise ValueError(f"do we get here 3 {release_tag}")
         for release in self.release_details:
             if release.release_tag == release_tag:
                 if len(release.image) > 1:
