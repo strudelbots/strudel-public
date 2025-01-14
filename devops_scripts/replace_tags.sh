5b43ca9823a3d197697e1d42535de5cd3c3096ba
shai <shai_rubin@yahoo.com>
2025-01-14 09:12:32 +0200

    replace tags

---
+++ devops_scripts/replace_tags.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x #echo on
+echo "jump tag"
+branch=$(git rev-parse --abbrev-ref HEAD)
+echo "branch is: $branch"
+if [ -z "$1" ]; then
+  echo "No parameter provided!"
+  exit 1
+fi
+if [ -z "$2" ]; then
+  echo "No second parameter provided!"
+  exit 1
+fi
+old_tag=$1
+new_tag=$2
+branch=$(git rev-parse --abbrev-ref HEAD)
+echo "old tag is: $old_tag"
+echo "new tag is: $new_tag"
+cd ../.github/workflows
+pwd
+find . -name "*.yml" -exec sed -i s/$old_tag/$new_tag/g {} \;
+
