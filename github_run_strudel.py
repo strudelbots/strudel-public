6ec4aab4747b880109d6852818c5689d57b24b9e
shai <shai_rubin@yahoo.com>
2025-02-16 08:15:29 +0200

    test-strudel fix filtering to remove __init__

--- github_run_strudel.py
+++ github_run_strudel.py
@@ -52,6 +52,7 @@ def get_all_files():
         print('content of the file of change files : '+ str(all_files))
         files = all_files.split(' ')
         for file in files:
+            print(f'file: {file}')
             if file.endswith('.py'):
                 python_files.append(file)
     return python_files, []
