ae08f7d7ba12f3374390036b3585303d621d2cf3
shai <shai_rubin@yahoo.com>
2025-02-16 08:12:15 +0200

    test-strudel fix filtering to remove __init__

--- github_run_strudel.py
+++ github_run_strudel.py
@@ -50,13 +50,8 @@ def get_all_files():
         with open(changed_files, 'r') as f:
             all_files = f.read()
         print('content of the file of change files : '+ str(all_files))
-        if len(all_files) != 1:
-            strudel.error(' Raise ValueError("ALL_CHANGED_FILES_FILE is empty") because Length of'
-            'all_files={len(all_files)} == 0')
-            raise ValueError('ALL_CHANGED_FILES is empty')
-        files = all_files[0].removesuffix('n')
-        print('content of files : '+ files)
-        for file in files.split(' '):
+        files = all_files.split(' ')
+        for file in files:
             if file.endswith('.py'):
                 python_files.append(file)
     return python_files, []
